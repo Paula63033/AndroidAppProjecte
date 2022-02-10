475436cd190ce42069e9387be2bbb6c5e644d604
alexr_4 <alexramon3000@gmail.com>
2022-02-10 17:03:45 +0100

    exemple ok

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void funcio(View e){
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView)findViewById(R.id.textExemple);
+        textView.setText("HelloWorld!");
+        textView.setTextSize(80);
+
+    }
+
+    public void funcio2(View e){
+        setContentView(R.layout.activity_main);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert exemple");
+        builder.setMessage("Hello world!");
+        builder.setPositiveButton("OK",null);
+        builder.create();
+        builder.show();
+
     }
 }
